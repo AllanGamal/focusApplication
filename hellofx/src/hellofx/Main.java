d8e21bbf06bbf9a358f5de7aed4858336cefa332
Allan Gamal <63316423+AllanGamal@users.noreply.github.com>
2023-02-13 20:36:51 +0100

    ,,...

--- hellofx/src/hellofx/Main.java
+++ hellofx/src/hellofx/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.show();
@@ -20,4 +20,6 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    
 }
