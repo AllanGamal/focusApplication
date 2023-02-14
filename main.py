b800621c88b45055c62004f0d25ae28ea27c1068
Allan Gamal <63316423+AllanGamal@users.noreply.github.com>
2023-02-14 15:03:15 +0100

    bug fixes

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ import select
 import sys
 
 safari = appscript.app("Safari")
-list = ["twitter", "facebook", "reddit", "youtube", "instagram", "tumblr", "pinterest", "imgur", "9gag"]
+keywords = ["twitter", "facebook", "reddit", "youtube", "instagram", "tumblr", "pinterest", "imgur", "9gag"]
 print("Enter 'append', 'remove', 'list' or 'exit': ")
 def printy():
     print("Enter 'append', 'remove', 'list' or 'exit': ")
 
 wait_for_delay = True  # start by waiting for the 3s delay
+command = ''  
+
 while True:
     # check if safari is running
     while not safari.isrunning():
         time.sleep(3)
     # Check tabs for keywords
     tabs = safari.windows.first.tabs()  # fetch latest tabs
-    for keyword in list:
+    for keyword in keywords:
         for tab in tabs:
             if tab.exists() and keyword in tab.URL():
                 tab.close()
@@ -34,24 +36,24 @@ while True:
             if listApp == "":
                 printy()
                 continue
-            elif listApp not in list:
-                list.append(listApp)
-            print(list)
+            elif listApp not in keywords:
+                keywords.append(listApp)
+            print(keywords)
             print(" ")
             printy()
         elif command == "remove": # Remove a keyword from the list
-            print(list)
+            print(keywords)
             print(" ")
             listRem = input("Remove the keyword: ")
             if listRem == "":
                 continue
-            elif listRem in list:
-                list.remove(listRem)
-            print(list)
+            elif listRem in keywords:
+                keywords.remove(listRem)
+            print(keywords)
             print(" ")
             printy()
         elif command == "list":
-            print(list)
+            print(keywords)
             print(" ")
             printy()
         else:
@@ -59,6 +61,6 @@ while True:
             print(" ")
             printy()
     else:
-        if wait_for_delay:
+        if wait_for_delay and not any(command == x for x in ['exit', 'append', 'remove', 'list']):
             time.sleep(3)  # wait for 3 seconds before checking Safari tabs again
         wait_for_delay = True  # reset the flag so that we wait for delay next time
